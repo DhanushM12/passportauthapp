5357dc94c93e05b8a07bb68b9f2fa3d9fc19a17a
DhanushM12 <dhanushmukhi@gmail.com>
2022-02-06 13:21:09 +0530

    config setup and app usage defined

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express = require("express");
 const app = express();
 const port = 8000;
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('express-flash');
+const methodOverride = require('method-override')
+const users = [];
+
+const initializePassport = require('./config/passportlocal');
+initializePassport(passport,
+    email => users.find(user => user.email == email,
+    id => users.find(user => user.id == id)   
+        ));
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false}));
+app.use(flash());
+app.use(session({
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
 
 app.listen(port, function(err){
     if(err){
